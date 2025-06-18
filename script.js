c38bcb995c5479a995949eb848f2b5a802137312
Adesh-pandey001 <119554829+Adesh-pandey001@users.noreply.github.com>
2025-06-18 08:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,24 +11,8 @@ const input = prompt("Enter a non-negative integer:");
 const num = parseInt(input);
 
 if (!isNaN(num) && num >= 0) {
-  alert(trailingZeros(num));
-} else {
-  alert("Invalid input. Please enter a non-negative integer.");
-}
-function trailingZeros(n) {
-  let count = 0;
-  while (n >= 5) {
-    n = Math.floor(n / 5);
-    count += n;
-  }
-  return count;
-}
-
-const input = prompt("Enter a non-negative integer:");
-const num = parseInt(input);
-
-if (!isNaN(num) && num >= 0) {
-  alert(trailingZeros(num));
+  const result = trailingZeros(num);
+  alert("Number of trailing zeros in " + num + "! is: " + result);
 } else {
   alert("Invalid input. Please enter a non-negative integer.");
 }
